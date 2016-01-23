4698553062034cf1bfc0a10c5b6c314e2dc7629b
Alex Buyanov <alex.bu.89@gmail.com>
2016-01-23 10:50:29 +0100

    http req ipdate

---
+++ app/src/main/java/com/proto/buddy/mountainbuddyv2/conf/AppConfig.java
@@ -0,0 +1,12 @@
+package com.proto.buddy.mountainbuddyv2.conf;
+
+/**
+ * Created by alex on 1/22/16.
+ */
+public class AppConfig {
+
+    public static String URL_REMOTE_SERVER = "http://78.47.217.227/mbapi/";
+
+    public static String URL_REMOTE_SERVER_ROUTES = "http://78.47.217.227/mbapi/routes/";
+
+}
