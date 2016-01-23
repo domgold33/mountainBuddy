8e095cc755590bd82f57b911ca376b9e862fca35
Alex Buyanov <alex.bu.89@gmail.com>
2016-01-23 23:38:54 +0100

    add HTTP request get put post delete, but not tested

--- app/src/main/java/com/proto/buddy/mountainbuddyv2/database/RemoteDatabaseHelper.java
+++ app/src/main/java/com/proto/buddy/mountainbuddyv2/database/RemoteDatabaseHelper.java
@@ -1,16 +1,23 @@
 package com.proto.buddy.mountainbuddyv2.database;
 
+import android.app.DownloadManager;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.proto.buddy.mountainbuddyv2.conf.AppConfig;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,20 +25,52 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RemoteDatabaseHelper {
 
-    private Context context;
-    private static String TAG = "----------- RemoteDatabaseHelper ----------------";
+    private static Context context;
+    private static String TAG = "---------------- RemoteDatabaseHelper ";
+    private static String URL_REMOTE_SERVER = "http://78.47.217.227/mountainbuddy/api/v1/";
+    private static String URL_REMOTE_SERVER_ROUTES = "http://78.47.217.227/mountainbuddy/api/v1/routes";
+    private static RequestQueue queue;
 
     public RemoteDatabaseHelper (Context context){
         this.context = context.getApplicationContext();
-        new HttpAsyncTask().execute("http://78.47.217.227/mbapi/routes/");
-        //GET(AppConfig.URL_REMOTE_SERVER_ROUTES);
+        queue = Volley.newRequestQueue(context);
+
+        GET(URL_REMOTE_SERVER_ROUTES);
+//        new HttpAsyncTask().execute(URL_REMOTE_SERVER_ROUTES);
     }
 
-    public static String GET(String urlToRead){
-        StringBuilder result = new StringBuilder();
+    public static void GET(String url){
+
+        Log.d(TAG, "starting get request to:  " + url);
+
+        // prepare the Request
+        JsonArrayRequest getRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONArray>()
+            {
+                @Override
+                public void onResponse(JSONArray response) {
+                    // display response
+                    Log.d(TAG, response.toString());
+                }
+            },
+            new Response.ErrorListener()
+            {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, "ERROR: " + error.getMessage());
+                }
+            }
+        );
+
+        // add it to the RequestQueue
+        queue.add(getRequest);
+
+        /*StringBuilder result = new StringBuilder();
 
         try {
             URL url = new URL(urlToRead);
@@ -47,23 +86,101 @@ public class RemoteDatabaseHelper {
         } catch (Exception e) {
             Log.d(TAG, e.toString());
         }
-        return result.toString();
+        return result.toString();*/
     }
 
-    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
-        String line = "";
-        String result = "";
-        while((line = bufferedReader.readLine()) != null)
-            result += line;
-        inputStream.close();
-        return result;
+    public static void POST(String url){
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+            new Response.Listener<String>()
+            {
+                @Override
+                public void onResponse(String response) {
+                    // response
+                    Log.d("Response", response);
+                }
+            },
+            new Response.ErrorListener()
+            {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // error
+                    Log.d(TAG, "ERROR: " + error.getMessage());
+                }
+            }
+        ) {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 
-    public class HttpAsyncTask extends AsyncTask<String, Void, String> {
+    public static void PUT(String url){
+        StringRequest putRequest = new StringRequest(Request.Method.PUT, url,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // error
+                        Log.d(TAG, "ERROR: " + error.getMessage());
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String>  params = new HashMap<String, String> ();
+                params.put("name", "Alif");
+                params.put("domain", "http://itsalif.info");
+
+                return params;
+            }
+
+        };
+
+        queue.add(putRequest);
+    }
+
+    public static void DELETE(String url){
+        StringRequest dr = new StringRequest(Request.Method.DELETE, url,
+            new Response.Listener<String>()
+            {
+                @Override
+                public void onResponse(String response) {
+                    // response
+                    Toast.makeText(context, response, Toast.LENGTH_LONG).show();
+                }
+            },
+            new Response.ErrorListener()
+            {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    // error.
+                    Log.d(TAG, "ERROR: " + error.getMessage());
+                }
+            }
+        );
+        queue.add(dr);
+    }
+
+    /*public class HttpAsyncTask extends AsyncTask<String, Void, String> {
         @Override
         protected String doInBackground(String... urls) {
-
             return GET(urls[0]);
         }
         // onPostExecute displays the results of the AsyncTask.
@@ -72,6 +189,6 @@ public class RemoteDatabaseHelper {
             Toast.makeText(context, "Received!", Toast.LENGTH_LONG).show();
             Log.d("Remote DB", "****************** " + result);
         }
-    }
+    }*/
 }
 
